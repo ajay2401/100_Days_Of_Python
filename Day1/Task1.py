8013cbcc3821781969683795613df9897ba7fe21
Ajay SS <ajay.ss@vlinkinfo.com>
2026-02-10 18:23:39 +0530

    Day 1 commit of notes and tasks

---
+++ Day1/Task1.py
@@ -0,0 +1,16 @@
+print("Hello " + input("Whats your name?") + "!")
+
+name = input("Whats your name")
+length = len(name)
+print(length)
+
+print("\n")
+
+# BAND NAME GENERATOR
+
+print("Welcome to Band Name Generator")
+city_name = input("What's the name of the city you grew up in?")
+print(city_name+"\n")
+pet_name = input("What's your pet's name?")
+print(pet_name+"\n")
+print("Your band name could be "+city_name+" "+pet_name)
